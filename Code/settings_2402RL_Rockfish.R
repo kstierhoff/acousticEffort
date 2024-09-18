7534d0634a7658e6f7444828f497efc30c55933a
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2024-09-18 11:48:16 -0700

    Added files for RREAS 2024 (2402RL).

---
+++ Code/settings_2402RL_Rockfish.R
@@ -0,0 +1,33 @@
+# Survey info -------------------------------------------------------------
+survey.name <- "2402RL"
+survey.vessel <- "Lasker"
+sd.survey   <- FALSE
+
+# Define ERDDAP data variables -------------------------------------------------
+erddap.survey.start  <- "2024-04-29"  # Start of survey for ERDDAP vessel data query
+erddap.survey.end    <- "2024-06-19"  # End of survey for ERDDAP vessel data query
+
+# Configure columns and classes
+erddap.vessel        <- "WTEG"    # Lasker == WTEG; Shimada == WTED; add "nrt" if during survey
+erddap.vars          <- c("time,latitude,longitude,seaTemperature,platformSpeed,flag")
+erddap.classes       <- c("character", "numeric", "numeric", "numeric","numeric","character")
+erddap.headers       <- c("time", "lat", "long", "SST", "SOG","flag")
+
+# Generate ERDDAP URL
+dataURL <- URLencode(paste0(
+  "http://coastwatch.pfeg.noaa.gov/erddap/tabledap/fsuNoaaShip",
+  erddap.vessel, ".csv0?", erddap.vars,
+  "&time>=", erddap.survey.start, "&time<=", erddap.survey.end,
+  "&flag=~",'"ZZ.*"'))
+
+# Downsample settings -----------------------------------------------------
+# Number of n-th samples to keep in the resulting nav data frame
+# Particularly important when dealing with large data sets with frequent
+# location estimates
+nav.ds <- 1
+
+# Map preferences ---------------------------------------------------------
+survey.zoom = 6
+map.height  = 8
+map.width   = 5
+map.type    = "hybrid"
